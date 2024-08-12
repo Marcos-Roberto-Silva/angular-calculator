8c1c5c28aec4e2475a3c7d96f99acd36b275eb59
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-08-12 14:51:48 -0400

    refactor to signal Angular 17 feature

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,6 +1,6 @@
-import {Component, output, Output, signal} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {FormsModule} from "@angular/forms";
-import {investmentInput} from "../models/investment-input.model";
+import {InvestmentService} from "../investment.service";
 
 @Component({
   selector: 'app-user-input',
@@ -10,19 +10,23 @@ import {investmentInput} from "../models/investment-input.model";
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  calculate = output<investmentInput>();
+
+  constructor(private InvestmentService: InvestmentService) { }
+
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
   enteredExpectedInvestment = signal('5');
   enteredDuration = signal('10');
 
+  investmentData = {
+    initialInvestment: Number(this.enteredInitialInvestment()),
+    duration:  Number(this.enteredDuration()),
+    expectedReturn: Number(this.enteredExpectedInvestment()),
+    annualInvestment: Number(this.enteredAnnualInvestment())
+  }
+
   onsubmit() {
-    this.calculate.emit({
-      initialInvestment: Number(this.enteredInitialInvestment()),
-      duration:  Number(this.enteredDuration()),
-      expectedReturn: Number(this.enteredExpectedInvestment()),
-      annualInvestment: Number(this.enteredAnnualInvestment())
-    });
+    this.InvestmentService.calculateInvestmentResults(this.investmentData);
 
     this.enteredInitialInvestment.set('0');
     this.enteredAnnualInvestment.set('0');
